de03252b9ff710ee3cbd7eb44b9a1395f2303a76
Bogdan Dumitrescu <bogdan.dumitrescu@getbux.com>
2017-06-09 14:57:34 +0200

    Ensure minikube is started before setting up rabbitmq

--- minikube/setup.sh
+++ minikube/setup.sh
@@ -10,6 +10,9 @@ minikube addons disable ingress
 # this is to counter clock skew when your laptop hibernates
 minikube ssh -- docker run -i --rm --privileged --pid=host debian nsenter -t 1 -m -u -n -i date -u $(date -u +%m%d%H%M%Y)
 
+#open the minikube dashboard (this will ensure that minikube running before executing the below commands)
+minikube dashboard
+
 # setup rabbitmq
 kubectl create secret generic rabbitmq-config --from-literal=erlang-cookie=2b3c4753-e8ee-487c-9564-619c4777c228
 kubectl apply -f rabbitmq.yaml
